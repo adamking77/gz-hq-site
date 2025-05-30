0121bac93bcbae51e59545f24ef54af198fa7104
Adam King <adamking77@gmail.com>
2025-05-30 23:15:06 +0400

    Initial backup of gz-hq-site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,25 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  markdown: {
+    drafts: true,
+    shikiConfig: {
+      theme: "css-variables"
+    }
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  shikiConfig: {
+    wrap: true,
+    skipInline: false,
+    drafts: true
+  },
+  site: 'https://yourdomain.com', // You may want to update this to your actual domain
+  integrations: [sitemap(), mdx()]
+});
